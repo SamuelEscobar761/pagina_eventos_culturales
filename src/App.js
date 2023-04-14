7a23f5507b2f6e7ca6fdbb7c90f0eb5c995c3196
Samuel Escobar <samueb8@gmail.com>
2023-04-14 11:08:50 -0400

    agregar  calendar a app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home_page/Home"
+import Calendar from "./pages/calendar_page/Calendar";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <Calendar />
     </div>
     
   );
