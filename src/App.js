f6a5e63f2ac25728da85badd541abf31c2ae082c
Samuel Escobar <samueb8@gmail.com>
2023-04-14 11:21:03 -0400

    Agregar enterprises y contact a app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Navbar from "./components/navbar/Navbar";
-import Home from "./pages/home_page/Home"
+import Home from "./pages/home_page/Home";
 import Calendar from "./pages/calendar_page/Calendar";
+import Enterprises from "./pages/enterprises_page/Enterprises";
+import Contact from "./pages/contact_page/Contact";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Navbar />
       <Home />
       <Calendar />
+      <Enterprises />
+      <Contact />
     </div>
     
   );
