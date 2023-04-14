20d4aaaad0679e07c1775c4b87ea8d4399582e03
Samuel Escobar <samueb8@gmail.com>
2023-04-14 11:16:47 -0400

    Crear el commponente de contact y su stylesheet

---
+++ src/pages/contact_page/Contact.js
@@ -0,0 +1,11 @@
+import "./css/style.css";
+
+function Contact(){
+    return(
+        <div className="contact-page" id="contact">
+            
+        </div>
+    );
+}
+
+export default Contact;
