309bbcd8a960659a1a57c736b9f0b35d80bce9ed
Samuel Escobar <samueb8@gmail.com>
2023-04-14 11:13:13 -0400

    Crear el componente enterprises y su stylesheet

---
+++ src/pages/enterprises_page/Enterprises.js
@@ -0,0 +1,11 @@
+import "./css/style.css";
+
+function Enterprises(){
+    return(
+        <div className="enterprises-page" id="enterprises">
+            
+        </div>
+    );
+}
+
+export default Enterprises;
